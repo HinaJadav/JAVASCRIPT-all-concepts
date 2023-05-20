85d2ef2c612af63aa228a0578fff756648b0c225
HinaJadav <105739881+HinaJadav@users.noreply.github.com>
2023-05-20 23:18:36 +0530

    Add files via upload

---
+++ Basic-JavaScript.js
@@ -0,0 +1,71 @@
+//DATATYPE : in JS there is a 8 datatypes
+    // undefined : when JS variable declares they have initial value of "undefine". (if we do mahematical operation on it we get --> NaN) & (if we concatenate string with undefine we get --> undefine)
+    // null
+    // boolean
+    // string
+    // symbol
+    // gidint
+    // number
+    // object
+
+//"var" keyword : we use it for declare variable
+// note:
+// 1) varibles name can be made up of --> numbers, letters, $, _
+// 2) varibles name not containt space and it is not start with space.
+// 3) for variable name we use camelCase method
+// 4) "JS is case sencitive laguage." so --> It is possible to have multiple distinct variables with the same name but different casing. 
+
+var myName;
+
+// "=" operator : for assign value to variable
+
+myName = "Priyank";
+
+//initializing variable
+var name = "xyz" // here name --> have string datatype
+
+// note : if we declare variables using "var" we can redeclare that variables and it leeds to --> ERROR --> solution of this is use "let"
+
+// "let" keyword: it is use for decalring varibles.
+
+// "const" keyword: it is use for declare read-only variables
+
+// "+" : addition 
+// "-" : substraction
+// "/" : divide
+// "*" : multiplication
+// "%" : reminder
+// "++" : increament variable value by 1
+// "--" : decreament variable value by 1
+// "+=" : Compound Assignment With Augmented addition
+// "-=" : Compound Assignment With Augmented substraction
+// "*=" : Compound Assignment With Augmented multiplication
+// "/=" : Compound Assignment With Augmented division
+
+// note : "\" we use it --> when we want to use any key-symbol as value (matalab uss keyword ki property ko nahi but use as value use karna chahte ho, for ex. agar hum "" kisi sentense me lagana chahte hoo. )
+
+// CONCATE STRINGS: 1) "+" op. 
+//                  2) "+=" op.
+
+//-----------STRING-------------
+// 1) length of string : (<string/ string name>.length)
+// 2) first character of string : <string/ string name>[0]
+
+// immutable value of string : In this we can't change any letter of string individually but we can change entire string value
+
+//---------------Array------------
+// some besic concepts follow here --> which we show before in c/c++
+// push() : it is use for append data at the end of an array.
+// pop() : it is use for remove last element of array.
+// shift() : it is use for remove first element of array.
+// unshift() : it is use for add first element in array.
+
+// ------------function--------------
+// Sysntex : function <function name>() {---code---}
+        // function reusableFunction() {  ---> declaration
+        //     console.log("Hi World");
+        // }
+        // reusableFunction(); ---> call/invoke function
+
+
+
