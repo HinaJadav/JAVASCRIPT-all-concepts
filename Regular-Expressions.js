d243ca8ded2cdb01ac5b74e667e3dbe07adda26a
HinaJadav <105739881+HinaJadav@users.noreply.github.com>
2023-06-02 23:18:37 +0530

    Update Regular-Expressions.js

--- Regular-Expressions.js
+++ Regular-Expressions.js
@@ -140,4 +140,87 @@
 
 
 
-//TOPIC 13: 
+//TOPIC 13: Lazy Matching
+// regex1 : (ex: /t[a-z]*i/) ==> this type of regex will return largest substring, which start with "t" end with "i" and some letters in between.
+// regex2 : (ex: /t[a-z]*?t/) ==> this type of regex will return smallest substring which start with "t" end with "i" and some letters in between.--------> ans. = ["ti"]
+
+
+
+
+
+//TOPIC 14: How to find multiple occurance of character into given string
+//regex : /<charcter>+/
+//Syntex : 'string'.match(regex);
+
+
+
+
+//TOPIC 15: Match Beginning String Patterns
+// regex : /^<string>/
+// ex:
+    // let firstString = "Ricky is first and can be found.";
+    // let firstRegex = /^Ricky/;
+    // firstRegex.test(firstString);--------------------------->true
+    // let notFirst = "You can't find Ricky now.";
+    // firstRegex.test(notFirst);------------------------------>false
+
+
+
+
+//TOPIC 16: Match Ending String Patterns
+// regex : /<string>$/
+// ex:
+    // let theEnding = "This is a never ending story";
+    // let storyRegex = /story$/;
+    // storyRegex.test(theEnding);----------------------------------->true
+    // let noEnding = "Sometimes a story will have to end";
+    // storyRegex.test(noEnding);------------------------------------>false
+
+
+
+
+// TOPIC 17: Match All Letters and Numbers Using "Match All Letters and Numbers".
+// "\w" == [A-Za-z0-9_](A to Z + a to z + 0 to 9 + _)
+// ex : 
+    // let longHand = /[A-Za-z0-9_]+/;
+    // let shortHand = /\w+/;
+    // let numbers = "42";
+    // let varNames = "important_var";
+    // longHand.test(numbers);--------------------->true
+    // shortHand.test(numbers);-------------------->true
+    // longHand.test(varNames);-------------------->true
+    // shortHand.test(varNames);------------------->true
+
+
+//NOTE : "\w" == [A-Za-z0-9_]
+//       "\W" == [^A-Za-z0-9_]
+//       "\d" == [0-9]
+//       "\D" == [^0-9]
+//       "\s" == " " or whitespace ("\s" is similer to [\r\t\f\n\v])
+        //ex: 
+            // let whiteSpace = "Whitespace. Whitespace everywhere!"
+            // let spaceRegex = /\s/g;
+            // whiteSpace.match(spaceRegex);---------------------------------->[" ", " "]
+//      "\S" == Non-Whitespace  ("\S" is similer to [^\r\t\f\n\v])
+        //ex:
+            // let whiteSpace = "Whitespace. Whitespace everywhere!"
+            // let nonSpaceRegex = /\S/g;
+            // whiteSpace.match(nonSpaceRegex).length;-------------------------> It return total no. of character in the string
+
+//      "+" == it look for one or more characters 
+//      "*" == it look for zero or more characters
+
+
+
+
+//TOPIC 18: Upper and Lower bound for Matches("Quantity specifier" == {x,y})----------------> Using this we can find match of specific character in limited range
+//regex : /<character>{x, y}/ (where, x = start range no. and y = end range no.)
+//ex : 
+      //to match only the letter a appearing between 3 and 5 times in the string -------------------> regex : /a{3,5}h/
+        // let A4 = "aaaah";
+        // let A2 = "aah";
+        // let multipleA = /a{3,5}h/;
+        // multipleA.test(A4);---------------------->true
+        // multipleA.test(A2);---------------------->false
+
+
