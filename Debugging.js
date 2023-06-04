16045d29ccfce332316c93c579027b791d428373
HinaJadav <105739881+HinaJadav@users.noreply.github.com>
2023-06-04 12:41:37 +0530

    Update Debugging.js

--- Debugging.js
+++ Debugging.js
@@ -1,2 +1,22 @@
 //Debugging : it is one kind of process in which we go throught our code & find issues and fixed them.
 //Issues type : 1)Syntex error, 2)runtime error, 3)logical error
+
+//TOPIC 1: print on browser
+
+//"console.log()" : print massage on browser
+//"console.clear()" : clear the browser console.
+
+
+
+
+//TOPIC 2: Check the Type of a Variables
+//use : typeof
+
+//ex:
+// console.log(typeof "");--------->String
+// console.log(typeof 0);---------->number
+// console.log(typeof []);--------->object
+// console.log(typeof {});--------->object
+
+//NOTE : JS recognize "seven" primitive datatype : Boolean, Null, Undefined, Number, String, Symbol (new with ES6), and BigInt (new with ES2020), Object
+
