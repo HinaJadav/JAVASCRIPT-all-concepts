2ced090277a0b736e5beba7dbfd73f5f4ebba2ca
HinaJadav <105739881+HinaJadav@users.noreply.github.com>
2023-07-09 23:23:33 +0530

    Add files via upload

---
+++ Function_Programming.js
@@ -0,0 +1,26 @@
+// function scope : INPUT -> PROCESS -> OUTPUT
+// Functional programming is about:
+    // Isolated functions - there is no dependence on the state of the program, which includes global variables that are subject to change
+    // Pure functions - the same input always gives the same output
+    // Functions with limited side effects - any changes, or mutations, to the state of the program outside the function are carefully controlled
+
+//Higher order function : The functions that take a function as an argument, or return a function as a return value
+    //When functions are passed in to or returned from another function, then those functions which were passed in or returned can be called a "lambda".
+
+//semantic error : means whatever we write in code not follow our thought for that
+//sneaky error : means when we use some feature and it gives unexpected output 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
