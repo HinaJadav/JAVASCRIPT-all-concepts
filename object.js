4c68fcd20234d84f847bdc1de58c35981782fd4d
HinaJadav <105739881+HinaJadav@users.noreply.github.com>
2023-07-30 00:57:19 +0530

    Update object.js

--- object.js
+++ object.js
@@ -1,4 +1,4 @@
-//Objetc:
+//Object:
 //non-primitive data types in JS
 // It hoalds multiple values innterms of properties and method
 // property = holds values of primitive datatype
@@ -194,8 +194,8 @@ for(var key in person2) {
 
 // Object.values(<object name>): This method return object properties values
 // return : array of object properties values
-    var value = Object.values(person2);
-    console.log(value);
+    // var value = Object.values(person2);
+    // console.log(value);
     // output:
     // [
     //     'John',
@@ -340,15 +340,224 @@ console.log(user2.id); // output: 4
 
 //----------------------------------------------------------------
 // Property Descriptor:
+// In object each property have property descriptor: it define nature of a property and
+// It retrive using: Ovject.getOwnPropertyDescriptor() method
+// Syntex : Object.getOwnPropertyDescriptor(<object name>, 'property name')
+//ex: 
+    var prop_descriptor = Object.getOwnPropertyDescriptor(person2, 'name');
+    console.log(prop_descriptor); 
+    // output:
+    // {
+    //     value: 'Nikita',
+    //     writable: true,
+    //     enumerable: true,
+    //     configurable: true
+    // }
+
+//* Property Attribute:
+// 1) value: cantains actual value of property
+// 2) writable: indicated wether property is writable or ReadOnly 
+        // writable = true (we can rewrite)
+        // writable = false (we can't rewrite)
+// 3) enumerable: indicates wether property is avilable inside for_loop for iteration.
+// 4) configurable: indicates wether property can be deleted or modify
+
+// writable, enumerable, configurable all are optional and bydefault "true"
+
+//----------------------------------------------------------------
+// Define properties:
+// allow user to define new property on an object.
+// also use for change descriptor of existing property
+
+// Syntex : Object.defineProperty(<object name>, descriptor);
+    Object.defineProperty(person2, 'Dharm', {writable: false});
+    var prop_descriptor1 = Object.getOwnPropertyDescriptor(person2, 'Dharm');
+    console.log(prop_descriptor1);
+    // output:
+    // {
+    //     value: 'Hindu',
+    //     writable: false,
+    //     enumerable: true,
+    //     configurable: true
+    // }
+    person2.Dharm = 'XYZ';
+    console.log(person2.Dharm); // output : hindu
+    
+    //ex:2
+    Object.defineProperty(person2, 'Dharm', {writable: true, configurable: false});
+    delete person2.Dharm; // it not work bcz, configuration = false
+    console.log(person2.Dharm);
+
+//----------------------------------------------------------------
+// Defining getters and setters:
+// ex: 
+    var user4 = {
+        name : 'John',
+        lname : "asd", 
+        get gname() {
+            return this.name + ' ' + this.lname;
+        },
+        set sname(value) {
+            [this.name, this.lname] = value.split(" ");
+            
+        }
+    }
+    user4.sname = "Hina Jadav";
+    console.log(user4.gname); //Hina Jadav
 
 
+// Add properties
+var obj = {counter: 0};
+Object.defineProperty(obj, "reset", {
+    get: function(value) {this.counter += value;}});
+
+var c = (obj.reset = 10);
+console.log(c); // 10
+
+//----------------------------------------------------------------
+var user5 = {
+    name : 'John',
+    lname : "asd", 
+    get: () => this.name.toUpperCase(),
+    set: value =>  {this.name = value;}
+}
+console.log(user5);
+    //output:
+    // {
+    //     name: 'John',
+    //     lname: 'asd',
+    //     get: [Function: get],
+    //     set: [Function: set]
+    // }
+
+user5.set("hina");
+console.log(user5.get()); //HINA
+
+//----------------------------------------------------------------
+// Accessor Descriptor
+// accesor : it have getters and setters methods
+// Object.defineProperty() : it have property values
+// property is either accesssor or data property not both
 
 
+//----------------------------------------------------------------
+//Enumerate the properties of an Object:
+//1) For..in loop : Iterates over all enumerable properties of an object.
+    for(var i in user5) {
+        console.log(i);
+    }
+    // output:
+    // name
+    // lname
+    // get
+    // set
+
+//2) Object.keys()
+    console.log(Object.keys(user5));
+    //output : [ 'name', 'lname', 'get', 'set' ]
+
+//3) Object.getOwnPropertyNames()
+    console.log(Object.getOwnPropertyNames(user5));
+    //output : [ 'name', 'lname', 'get, set' ]
 
 
+//----------------------------------------------------------------
+// "this" kryword:
+// it points peticuler object.(here, which object we refers it depends on how function called that object)
+
+// Rule of use "this"
+// 1) if we use this alone ---> it points Global scope. & work as refernce of window object
+    var x = this;
+    console.log("x:" + x); // [object Window]
+
+    //ex:2
+    var n = 1000;
+    function number() {
+        var n = 100;
+        console.log("n = " + n); 
+        console.log("this.n = " + this.n);
+    }
+    number();
+    // (see output on console)
+    //n = 100 -----> refers local variable
+    //this.n = 1000 ------> refer global variable
+//Note : 
+// So, if 'this' is used inside any global function and called without dot 
+// notation or using window. then this will refer to global object which is 
+// default window object.
 
 
+//2) "this" Inside Object method:
+//if we use this with object ---> refernce of that object
+    var y = {
+        name: "asd"
+    }
+    console.log(this.y); 
+    // {
+    //     "name": "asd"
+    // } 
 
+// When we create object using "new" keyword then ---> "this" refers that object
+    var n1 = 200;
+    function number1() {
+        this.n1 = 100;
+    }
+    var num = new number1();
+    console.log(num.n1); //100
+
+    var num1 = new number1();
+    num1.n1 = 600;
+    console.log(num1.n1); //600
+
+//3) call() & apply():
+// Main purpose of use call() and apply() method is to set the context of this inside a function  
+// irrespective whether that function is being called in the global scope or as object's method
+
+var fname = "MM";
+function Women() {
+    console.log(this.fname);
+}
+
+// create object of "Window class (global class)"
+var o1 = {fname : "Madhu", Women: Women}; 
+var o2 = {fname : "Anjali", Women: Women};
+
+Women();// (because it call globally so this refers global variable)
+ // MM
+Women.call(o1); // this points o1
+ // Madhu
+Women.apply(o2); // this points o2
+ // Anjali
+
+o1.Women.call(window); // this points window
+ // MM
+
+
+// bind() :used to set the context of 'this' to a specified object when a function is invoked
+// *usually helpful in setting up the context of this for a callback function.
+
+
+function bahen(callback) {
+    var fname = "YY";
+    callback();
+};
+var o3 = {
+    fname : "WW",
+    show: function() {
+        console.log(this + ":" + this.fname);
+    }
+};
+bahen(o3.show); // [object window]:MM
+bahen(o3.show.bind(o3)); // [object Object]:WW
+
+// How "new" key word work behind the seen for creation of new object.
+// 1) it create empty object ---> {}
+// 2) assign function prototypr ---> {__proto__ = <function name>.prototype}
+// 3) assign properties and methods declare with "this" keyword ---> {__proto__ = <function name>.protutype, <properties with their values>}
+// 4) return newly created object ---> var obj =  {__proto__ = <function name>.protutype, <properties with their values>}
+
+// if "new" keyword retur "primitive" values ---> then it can perform following 4 task
+// if "new" keyword retur "non-primitive" values ---> then it can't perform following 4 task
 
 
 
